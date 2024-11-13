06412174755f305198a557bb082f3abcb37192bd
madeinindia <154547584+webpages1@users.noreply.github.com>
2024-11-13 21:48:04 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,45 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// int row1,row2,col1,col2;
+// void SumOfMatrix(int mat1[][col1],int mat2[][col1],int row,int col){
+//     int ans[row][col];
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             ans[i][j]=mat1[i][j]+mat2[i][j];
+//         }
+//     }
+//     for(int i=0;i<row;i++){
+//         printf("| ");
+//         for(int j=0;j<col;j++){
+//             printf("%d ",ans[i][j]);
+//         }
+//         printf("|\n");
+//     }
+// }
+// int main(){
+//     printf("Enter the number of rows and columns in first matrix");
+//     scanf("%d%d",&row1,&col1);
+//     printf("Enter the number of rows and columns in second matrix");
+//     scanf("%d%d",&row2,&col2);
+//     int matrix1[row1][col1];
+//     int matrix2[row2][col2];
+//     printf("enter the values of first matrix");
+//     for(int i=0;i<row1;i++){
+//         for(int j=0;j<col1;j++){
+//             scanf("%d",&matrix1[i][j]);
+//         }
+//     }
+//     printf("enter the values of second matrix");
+//     for(int i=0;i<row2;i++){
+//         for(int j=0;j<col2;j++){
+//             scanf("%d",&matrix2[i][j]);
+//         }
+//     }
+//     SumOfMatrix((int *)matrix1,(int *)matrix2,row1,col1);
+//     printf("Everything happend successfully");
+//     return 0;
+// }
+
+
+
